f36e28eb3e80d49fe9a6af36c1a8e7d6fdc317b1
Danilo Ferreira <danilocsf@gmail.com>
2021-10-20 17:03:07 -0300

    Outputing conditional elements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,24 +12,50 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => { 
-    return expense.date.getFullYear().toString() === filteredYear 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
   });
+
+  //Uma variável pode armazenar código JSX
+  let expenseContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map(expense =>
+      <ExpenseItem
+        key={expense.id} //Essa key deve ser única, sem essa propriedade podem surgir bugs
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    )
+  }
+
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map(expense =>
-          <ExpenseItem
-            key={expense.id} //Essa key deve ser única, sem essa propriedade podem surgir bugs
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        )}
+        {expenseContent}  
       </Card>
     </div>
+
+    //Essa é outra forma de fazer a condição para a exibição da mensagem
+    // <div>
+    //   <Card className="expenses">
+    //    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
+    //     {filteredExpenses.length === 0 && <p>No expenses found.</p> /*o que vem após && é executado se a condição for true*/}
+    //     {filteredExpenses.length > 0 &&
+    //       filteredExpenses.map(expense =>
+    //         <ExpenseItem
+    //           key={expense.id} //Essa key deve ser única, sem essa propriedade podem surgir bugs
+    //           title={expense.title}
+    //           amount={expense.amount}
+    //           date={expense.date}
+    //         />
+    //       )
+    //     }
+    //   </Card>
+    // </div>
   );
 }
 
