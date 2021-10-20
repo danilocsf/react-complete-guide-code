29bf31e38b6db0c1f3f40e5711572bf9e32cf04d
Danilo Ferreira <danilocsf@gmail.com>
2021-10-20 11:22:55 -0300

    Ajustando ExpenseItem.js pois estava bagunçando a lista

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,22 +5,16 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  
-  //Primeiro valor é o valor a ser alterado, o segundo a função que altera esse valor
-  const [title, setTitle] = useState(props.title);
-  
-  const clickHandler = () => {
-    setTitle('Updated');
-  }
-
+ 
+ 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Tilte</button>
+      
     </Card>
   );
 }
