17236e31d2ab5a29fe67074d9efa81d00b352be1
Danilo Ferreira <danilocsf@gmail.com>
2021-10-25 11:18:36 -0300

    Managing the Error State

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -8,14 +8,23 @@ import ErrorModal from '../UI/ErrorModal';
 const AddUser = (props) => {
     const [enteredUserName, setEnteredUserName] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please inform a value for username and age.'
+            });
             return;
         }
         //Colocar + na frente da variavel força a transformar em numero
         if (+enteredAge < 1) {
+            setError({
+                title: 'Invalid age',
+                message: 'Please inform a valid age (>0).'
+            });
             return;
         }
         props.onAddUser(enteredUserName.trim(), enteredAge.trim());
@@ -31,13 +40,20 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
         /* a propriedade className foi definida em Card => ${props.className}`}
             Isso foi feit para que aqui neste component, ou qualquer component filho de card
             pudessem ser utilizados CSSs não definidos no component card, como por exemplo o input abaixo
          */
         <div>
-            <ErrorModal title={"An error occurred!"} message={"Something went wrogn!"} />
+            {error && <ErrorModal 
+                        title={error.title} 
+                        message={error.message} 
+                        onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
