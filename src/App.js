a06ce916ca35f87b61f7705d7cb7d13804ff6691
Danilo Ferreira <danilocsf@gmail.com>
2021-10-29 09:37:23 -0300

    Using the React Context API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import Authcontext from './Context/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -30,13 +31,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    /*<Authcontext.Provider> - todos os componentes que estão abaixo dessa tag, terão acesso ao auth context*/
+    <Authcontext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </Authcontext.Provider>
   );
 }
 
