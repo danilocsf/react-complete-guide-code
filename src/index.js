9de8a3251c67e1e1c3866e296a74e5cb1aba85d1
Danilo Ferreira <danilocsf@gmail.com>
2021-11-01 13:24:12 -0300

    Starting project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './Context/auth-context';
 
-ReactDOM.render(<AuthContextProvider><App /></AuthContextProvider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
